a424c5c838916dcba9c7a167e45cb093009b5620
Anna Ramos Triguero <anna.ramos.triguero@gmail.com>
2021-06-06 12:38:57 +0200

    ILSE1 Project

---
+++ Terminal-Script.sh
@@ -0,0 +1,62 @@
+# cd /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/
+
+#CAL29
+# STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS31/RS31_1.fq.gz /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS31/RS31_2.fq.gz  --runThreadN 22  --outFileNamePrefix Cal29_siCtrl438_1_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+# STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS32/RS32_1.fq.gz /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS32/RS32_2.fq.gz  --runThreadN 22  --outFileNamePrefix Cal29_siCtrl438_2_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+# STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS33/RS33_1.fq.gz /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS33/RS33_2.fq.gz  --runThreadN 22  --outFileNamePrefix Cal29_siCtrl438_3_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+# STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS34/RS34_1.fq.gz /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS34/RS34_2.fq.gz  --runThreadN 22  --outFileNamePrefix Cal29_si270_1_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+# STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS35/RS35_1.fq.gz /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS35/RS35_2.fq.gz  --runThreadN 22  --outFileNamePrefix Cal29_si270_2_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+# STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS36/RS36_1.fq.gz /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/X204SC20090128-Z01-F001_1/raw_data/RS36/RS36_2.fq.gz  --runThreadN 22  --outFileNamePrefix Cal29_si270_3_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+
+
+cd /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric
+
+#5637
+STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS1/RS1_1.fq.gz /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS1/RS1_2.fq.gz  --runThreadN 22  --outFileNamePrefix 5637_siCtrl438_1_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS2/RS2_1.fq.gz /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS2/RS2_2.fq.gz  --runThreadN 22  --outFileNamePrefix 5637_siCtrl438_2_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS3/RS3_1.fq.gz /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS3/RS3_2.fq.gz  --runThreadN 22  --outFileNamePrefix 5637_siCtrl438_3_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS4/RS4_1.fq.gz /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS4/RS4_2.fq.gz  --runThreadN 22  --outFileNamePrefix 5637_si270_1_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS5/RS5_1.fq.gz /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS5/RS5_2.fq.gz  --runThreadN 22  --outFileNamePrefix 5637_si270_2_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+STAR --genomeDir /data/applications/STAR-master/Genomes/hg19 --readFilesIn /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS6/RS6_1.fq.gz /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/raw/X201SC20080988-Z01-F001/raw_data/RS6/RS6_2.fq.gz  --runThreadN 22  --outFileNamePrefix 5637_si270_3_  --readFilesCommand gunzip -c  --outSAMtype BAM SortedByCoordinate  --limitBAMsortRAM 60000000000
+
+echo "finished_mapping." | mutt -s "Subject End of DEGs" anna.ramos-triguero@uniklinik-freiburg.de
+
+
+# mkdir /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/fastQC_folder
+
+# cd /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/
+
+# fastqc -o /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/fastQC_folder -f bam -t 20 Cal29_si270_1_Aligned.sortedByCoord.out.bam
+# fastqc -o /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/fastQC_folder -f bam -t 20 Cal29_si270_2_Aligned.sortedByCoord.out.bam
+# fastqc -o /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/fastQC_folder -f bam -t 20 Cal29_si270_3_Aligned.sortedByCoord.out.bam
+# fastqc -o /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/fastQC_folder -f bam -t 20 Cal29_siCtrl438_1_Aligned.sortedByCoord.out.bam
+# fastqc -o /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/fastQC_folder -f bam -t 20 Cal29_siCtrl438_2_Aligned.sortedByCoord.out.bam
+# fastqc -o /data/calculations/RNAseq_hg19_Cal29_HepG2_5637_HT1376_siCT_si270_KMI441_Eric/fastQC_folder -f bam -t 20 Cal29_siCtrl438_3_Aligned.sortedByCoord.out.bam
+
+cd /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric
+
+mkdir /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/fastQC_folder
+
+fastqc -o /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/fastQC_folder -f bam -t 20 5637_siCtrl438_1_Aligned.sortedByCoord.out.bam
+fastqc -o /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/fastQC_folder -f bam -t 20 5637_siCtrl438_2_Aligned.sortedByCoord.out.bam
+fastqc -o /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/fastQC_folder -f bam -t 20 5637_siCtrl438_3_Aligned.sortedByCoord.out.bam
+fastqc -o /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/fastQC_folder -f bam -t 20 5637_si270_1_Aligned.sortedByCoord.out.bam
+fastqc -o /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/fastQC_folder -f bam -t 20 5637_si270_2_Aligned.sortedByCoord.out.bam
+fastqc -o /data/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/fastQC_folder -f bam -t 20 5637_si270_3_Aligned.sortedByCoord.out.bam
+
+
+echo "finished_fastqc." | mutt -s "Finished FASTQC analysis" anna.ramos-triguero@uniklinik-freiburg.de
+
+
+#MULTIQC
+
+#Be out of the server because MultiQC is installed in my Mac
+exit
+
+#Go to the server from my computer to take the data from there
+#Deberia poner todo en la misma carpeta
+cd /Volumes/calculations/RNASeq_hg19_Bladder_5673_HT1376_Cal29_siCT_si270_si270_KMI441_Eric/fastQC_folder
+
+multiqc .
+
+echo "finished_multiqc." | mutt -s "Finished MULTIQC analysis" anna.ramos-triguero@uniklinik-freiburg.de
